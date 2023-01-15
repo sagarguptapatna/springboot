8965037b388b62edff2c991fc97427c16f25a775
Roushan Kumar <pnberoushan@outlook.com>
2023-01-15 15:37:05 +0530

    adding project

---
+++ accuator/src/main/java/com/roushan/accuator/AccuatorApplication.java
@@ -0,0 +1,13 @@
+package com.roushan.accuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccuatorApplication.class, args);
+	}
+
+}
---
+++ accuator/src/test/java/com/roushan/accuator/AccuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roushan.accuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
